fe13f20d56a9afa4fd0b52bb5c5db28ac2ad9edc
Matt Bilker <me@mbilker.us>
2012-09-01 22:30:46 -0400

    Putting back on heroku

--- config.js
+++ config.js
@@ -1,15 +1,12 @@
 exports.dev = {
     port: 3000,
     client_port: 3000,
-    //mongoose_auth: 'mongodb://mongodb@localhost/subway'
     mongoose_auth: 'mongodb://subway:subway@alex.mongohq.com:10002/Subway'
 }
 
 exports.prod = {
-    //port: 14858, // Nodester port
     port: process.env.PORT || 5000,
-    client_port: 80, // Websockets talk on port 80 on Nodester, regardless of listen port
-    //mongoose_auth: 'mongodb://mongodb@localhost/subway'
+    client_port: 80,
     mongoose_auth: 'mongodb://subway:subway@alex.mongohq.com:10002/Subway'
 }
 
